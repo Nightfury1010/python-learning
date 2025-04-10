461a9a73263b7603e991574e5669a2242b169042
Nightfury1010 <roshanpandey72106@gmail.com>
2025-04-10 20:28:24 +0545

    basic learning

--- tut_2.py
+++ tut_2.py
@@ -1,56 +1,56 @@
-# # type conversion 
-# num= input ("Enter your birth year : ")
-# age = 2025- int(num)
-# print("Your age is : ", age)    
+# type conversion 
+num= input ("Enter your birth year : ")
+age = 2025- int(num)
+print("Your age is : ", age)    
 
-# mass = input("Enter your mass in lb : ")
-# maa_kg = float(mass) * 0.453592
-# print("Your mass in kg is : ", maa_kg)
-
-
-# # introduction to String
-# string= 'he said-"gasascascasc"'
-# print(string)
-# print(string[0])
+mass = input("Enter your mass in lb : ")
+maa_kg = float(mass) * 0.453592
+print("Your mass in kg is : ", maa_kg)
 
 
-# #formatted string
-# first= 'John'
-# last= 'Doe'
-# msg= f'Hello {first} [{last}], welcome to the world of python'
-# print(msg)
+# introduction to String
+string= 'he said-"gasascascasc"'
+print(string)
+print(string[0])
 
-# #string methods
-# str= 'welcome to Python'    
-# print(len(str))
-# print(str.upper())
-# print(str.lower())
-# print(str.find('P'))
-# print(str.replace('Python', 'Java'))
-# print('python' in str)
-# print('Python' in str)
 
-# #arithmetc operations
-# print(2+3)
-# print(2-3)
-# print(2*3)
-# print(2/3)
-# print(2//3)
-# print(2%3)
-# print(2**3)
-# x=10
-# x=x+3
-# print(x)
-# x+=3
-# print(x)
-# x-=3
-# print(x)
+#formatted string
+first= 'John'
+last= 'Doe'
+msg= f'Hello {first} [{last}], welcome to the world of python'
+print(msg)
 
-# #operator precedence
-# x=2+3*4
-# print(x)
+#string methods
+str= 'welcome to Python'    
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.find('P'))
+print(str.replace('Python', 'Java'))
+print('python' in str)
+print('Python' in str)
 
-# #math Functions
-# import math
-# print(math.ceil(2.1))
-# print(math.floor(2.9))
+#arithmetc operations
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
+print(2%3)
+print(2**3)
+x=10
+x=x+3
+print(x)
+x+=3
+print(x)
+x-=3
+print(x)
+
+#operator precedence
+x=2+3*4
+print(x)
+
+#math Functions
+import math
+print(math.ceil(2.1))
+print(math.floor(2.9))
