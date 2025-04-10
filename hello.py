f03d56a837f561a4a837f3d555462a8290830d98
Nightfury1010 <roshanpandey72106@gmail.com>
2025-04-10 17:53:35 +0545

    tutorial

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 print("hello world")
+print('0-----')
+print(' ||||')
+print('* ' * 10)
+
+price = 10  
+price = 20
+print(price)
+
+
+name = 'john smith'
+age= 20
+is_a_new_patient = True 
+print(name + ' is ' + str(age) + ' years old')
+
+input('Enter your name: ')
+name = input('Enter your name: ')
+print('Hello ' + name)
